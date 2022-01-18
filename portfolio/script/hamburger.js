212cc9dfd49e29fd1e6c9a7c6dbc31b37fe9317c
Danil Gordeev <dan.gor220@yandex.ru>
2022-01-18 13:37:49 +0300

    feat: add script for hamburger

--- portfolio/script/hamburger.js
+++ portfolio/script/hamburger.js
@@ -1,6 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
+const listNavigation = document.querySelector('.header__list');
 
 hamburger.addEventListener('click', ()=> {
   hamburger.classList.toggle('is-active');
+  listNavigation.classList.toggle('open');
 });
 
+listNavigation.addEventListener('click', closeMenu);
+
+function closeMenu(event) {
+  if(event.target.classList.contains('header__list-item') || event.target.classList.contains('header__list-link')) {
+    hamburger.classList.remove('is-active');
+    listNavigation.classList.remove('open');
+  };
+}
