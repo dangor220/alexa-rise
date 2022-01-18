461c8be0600a0a5107691635351f38b055767417
Danil Gordeev <dan.gor220@yandex.ru>
2022-01-18 16:16:32 +0300

    fix: .content__adaptive-menu

--- portfolio/script/hamburger.js
+++ portfolio/script/hamburger.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const listNavigation = document.querySelector('.header__list');
+const listNavigationTwo = document.querySelector('.content__adaptive-menu');
 
 hamburger.addEventListener('click', ()=> {
   hamburger.classList.toggle('is-active');
   listNavigation.classList.toggle('open');
+  listNavigationTwo.classList.toggle('open');
 });
 
 listNavigation.addEventListener('click', closeMenu);
