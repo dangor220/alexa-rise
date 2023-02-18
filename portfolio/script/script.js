a9842277d9a67ae104a7aa357dd801cce80e619d
Danil Gordeev <50300277+dangor220@users.noreply.github.com>
2023-02-19 01:45:50 +0300

    fix: delete evaluating

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -1,25 +1,3 @@
-console.log(`
-
-  Итоги по выполненной работе:
-    1.	Смена изображений в секции portfolio +25
-
-    2.	Перевод страницы на два языка +25
-  
-    3.	Переключение светлой и тёмной темы +25
-    4.	Дополнительный функционал: выбранный пользователем язык отображения страницы и светлая или тёмная тема сохраняются при перезагрузке страницы +5
-    5.	Дополнительный функционал: сложные эффекты для кнопок при наведении и/или клике +5
-        o	Ripple Button Демо
-        o	Fluent Button Демо
-        o	Particles Button Демо
-        o	Bubbly Button Демо
-        o	Button Hover Effects Демо
-        o	Box-shadow Button Демо
-  
-    
-  Сумма баллов: 85
-  
-`);
-
 let btnSubmit = document.querySelector(".button_size_submit");
 
 btnSubmit.addEventListener("click", function (event) {
